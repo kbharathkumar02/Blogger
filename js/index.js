9bb8fdbcd265c715c333acb46af59c1880ef8281
Kadavergu Bharath Kumar <kbharathkumar02@gmail.com>
2019-04-11 18:57:28 +0530

    Blogger-3: Updated post buttons.

--- js/index.js
+++ js/index.js
@@ -12,12 +12,6 @@ span.onclick = function() {
     modalCreatePost.style.display = "none";
 }
 
-window.onclick = function(event) {
-    if (event.target == modalCreatePost) {
-        modalCreatePost.style.display = "none";
-    }
-}
-
 document.getElementById("allPosts").onclick = function () {
     location.href = "/Blogger/html/bloglist.html";
 };
